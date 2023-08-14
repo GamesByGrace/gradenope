497cbeefd640b09c3ac22a7d9d2f6bd725ade36d
Grace <120279661+GamesByGrace@users.noreply.github.com>
2023-08-14 16:06:37 -0400

    added click-to-view functionality

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,7 @@
 const c = document.getElementsByClassName("submissionStatus--score");
 for (let i = 0; i < c.length; i++) {
-    c[i].innerHTML = "Graded";
+    var score = c[i].innerHTML;
+    var htmlElem = "<details><summary>Graded</summary><p>" + score + "</p></details>";
+
+    c[i].innerHTML = htmlElem;
 }
