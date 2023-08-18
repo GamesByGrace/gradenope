53892cd1cb0a141ea4a7e413793cf10524d26ad0
Grace <120279661+GamesByGrace@users.noreply.github.com>
2023-08-17 21:28:20 -0400

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,5 @@
 const c = document.getElementsByClassName("submissionStatus--score");
+
 for (let i = 0; i < c.length; i++) {
     var score = c[i].innerHTML;
     var htmlElem = "<details><summary>Graded</summary><p>" + score + "</p></details>";
