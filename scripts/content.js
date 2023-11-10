4f7414192c6cb6f15fc7a3ce2bb30dc5e81f844a
Grace <gracedes@bu.edu>
2023-11-10 13:40:49 -0500

    unified font

--- scripts/content.js
+++ scripts/content.js
@@ -2,7 +2,7 @@ const c = document.getElementsByClassName("submissionStatus--score");
 
 for (let i = 0; i < c.length; i++) {
     var score = c[i].innerHTML;
-    var htmlElem = "<details><summary>Graded</summary><p>" + score + "</p></details>";
+    var htmlElem = "<details><summary class='submissionStatus--text'>Graded</summary><p>" + score + "</p></details>";
 
     c[i].innerHTML = htmlElem;
 }
