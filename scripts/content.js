a4153f6b6e298a2780e0dc3e63708067484f469a
Grace <gracedes@bu.edu>
2023-11-28 10:06:34 -0500

    added percent calculation

--- scripts/content.js
+++ scripts/content.js
@@ -2,7 +2,11 @@ const c = document.getElementsByClassName("submissionStatus--score");
 
 for (let i = 0; i < c.length; i++) {
     var score = c[i].innerHTML;
-    var htmlElem = "<details><summary class='submissionStatus--text'>Graded</summary><p>" + score + "</p></details>";
+    
+    var scoreArr = score.split(" ");
+    var percentScore = (parseFloat(scoreArr[0]) / parseFloat(scoreArr[2])) * 100;
+
+    var htmlElem = "<details><summary class='submissionStatus--text'>Graded</summary><p>" + score + "&nbsp;&nbsp;(" + percentScore.toFixed(2) + "%)" + "</p></details>";
 
     c[i].innerHTML = htmlElem;
 }
